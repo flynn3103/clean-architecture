2c04aa246e16455b20e506f1cc8ff82d0609a0cf
linh <trannhatlinh3103@gmail.com>
2025-02-07 07:57:55 +0700

    Add currency exchange rate time series use case and repositories

--- src/interface/repositories/exchange_rate.py
+++ src/interface/repositories/exchange_rate.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from django.db.models.query import QuerySet
 
 from src.domain.exchange_rate import CurrencyEntity, CurrencyExchangeRateEntity
@@ -23,6 +25,11 @@ class CurrencyExchangeRateRepository:
     def get(self, source_currency: str, exchanged_currency: str,
             valuation_date: str) -> CurrencyExchangeRateEntity:
         return self.db_repo.get(source_currency, exchanged_currency, valuation_date)
+
+    def get_rate_series(self, source_currency: str, exchanged_currency: str,
+            date_from: str, date_to: str) -> QuerySet[CurrencyExchangeRateEntity]:
+        return self.db_repo.get_time_series(
+            source_currency, exchanged_currency, date_from, date_to)
 
     def get_time_series(self, source_currency: str, exchanged_currency: str,
             date_from: str, date_to: str) -> QuerySet[CurrencyExchangeRateEntity]:
--- src/usecases/exchange_rate.py
+++ src/usecases/exchange_rate.py
@@ -1,5 +1,9 @@
 # coding: utf-8
 
+from typing import List
+
+import pandas as pd
+
 from django.db.models.query import QuerySet
 
 from src.domain.exchange_rate import CurrencyEntity, CurrencyExchangeRateEntity
@@ -15,7 +19,8 @@ class CurrencyInteractor:
 
     def get_availables(self) -> QuerySet[CurrencyEntity]:
         return self.currency_repo.get_availables()
-            
+
+
 class CurrencyExchangeRateInteractor:
 
     def __init__(self, exchange_rate_repo: object):
@@ -31,7 +36,16 @@ class CurrencyExchangeRateInteractor:
         today = pd.Timestamp.today().strftime('%Y-%m-%d')
         return self.get(source_currency, exchanged_currency, today)
 
+    def get_rate_series(self, source_currency: str, exchanged_currency: str,
+            date_from: str, date_to: str) -> List[float]:
+        return self.exchange_rate_repo.get_rate_series(
+            source_currency, exchanged_currency, date_from, date_to)
+
     def get_time_series(self, source_currency: str, exchanged_currency: str,
             date_from: str, date_to: str) -> QuerySet[CurrencyExchangeRateEntity]:
         return self.exchange_rate_repo.get_time_series(
             source_currency, exchanged_currency, date_from, date_to)
+
+    def get_all_time_series(self, source_currency: str, date_from: str,
+            date_to: str) -> QuerySet[CurrencyExchangeRateEntity]:
+        return self.get_time_series(source_currency, None, date_from, date_to)
